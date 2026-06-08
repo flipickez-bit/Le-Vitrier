74fe5b4fc4d7d622888a02ca269a282629539b54
flipickez-bit <flipickez@gmail.com>
2026-06-08 20:41:07 +0200

    Revert -f (incompatible Hostinger), retour version fonctionnelle

--- contact.php
+++ contact.php
@@ -73,7 +73,7 @@ if (count($attachments) === 0) {
   $headers .= $replyTo;
   $headers .= "MIME-Version: 1.0\r\n";
   $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-  $ok = @mail($to, $subjectEnc, $body, $headers, '-fno-reply@levitrier06.fr');
+  $ok = @mail($to, $subjectEnc, $body, $headers);
 } else {
   /* Email multipart avec pièces jointes */
   $boundary = '=_lv_' . md5(uniqid((string) time(), true));
@@ -96,7 +96,7 @@ if (count($attachments) === 0) {
   }
   $msg .= "--" . $boundary . "--";
 
-  $ok = @mail($to, $subjectEnc, $msg, $headers, '-fno-reply@levitrier06.fr');
+  $ok = @mail($to, $subjectEnc, $msg, $headers);
 }
 
 if ($ok) {
